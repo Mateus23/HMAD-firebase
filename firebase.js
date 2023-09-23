d0df768c22a573f12a1e0eb9aead1dc3255b5817
Mateus Pelluchi <mateus.pelluchi@gmail.com>
2023-09-23 15:17:04 -0300

    firebase base

---
+++ firebase.js
@@ -0,0 +1,5 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+    webClientId: '155337950601-7ejc8dt5a6p645oul0a3t9h7ga0bb01b.apps.googleusercontent.com',
+});
